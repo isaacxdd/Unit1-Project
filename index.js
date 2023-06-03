8e55eb127d1d23d04b8773ea75384303094d5527
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-06-03 09:11:12 -0400

    API

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(() => {
     let $h1 = $('<h1>').text('Marvel');
     $title.append($h1);
     
-    ///////////////////////
+///////////////////////////////////
     
     let $navbar =$('.navbar');
     
@@ -16,3 +16,55 @@ $(() => {
      
     });
     
+
+/////////////////////////////////
+
+const $searchForm = $("form");
+
+$searchForm.on("submit", event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const anime = formData.get("anime");
+
+    const url = `https://gateway.marvel.com/v1/public/characters?name=${anime}&ts=1&&apikey=a459e244b568f5a455ec8c997cd28970&hash=6368c925608e3f9b51a06abc10dcb485`
+    console.log(url);  
+ 
+    ////////////////////////////////////////////////////////////////////////
+
+    const $screen = $(".screen");
+    const $result = $(".result");
+
+    //empty out the input field
+    $('[name="anime"]')[0].value = "";
+
+    // empty out previous result and add in a loading indicator
+    $screen.empty();
+    $result.html(`<div>Loading...</div>`)
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            $screen.html(
+                `<img src=${data.data.results[0].thumbnail.path + "." + "jpg"}>`
+            );
+            $result.html(`
+                <div>
+                    <b>name:&nbsp;</b> ${data.data.results[0].name}
+                </div>
+
+                <div>
+                    <b>id:&nbsp;</b> ${data.data.results[0].id}
+                </div>
+
+                <div>
+                    <b>description:&nbsp;</b> ${data.data.results[0].description}
+                </div>
+            `)
+        })
+        .catch(() => {
+            $result.html(`<div> there was an error...</div>`)
+        });
+});
